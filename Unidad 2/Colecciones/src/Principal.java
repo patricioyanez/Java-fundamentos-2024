71c95e2e0984b19303d395cc98ae63b39790989a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-09 11:05:43 -0300

    coleccion

---
+++ Unidad 2/Colecciones/src/Principal.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        // crear coleccion que soporta objetos String
+        ArrayList<String> nombres;
+        nombres = new ArrayList<String>();
+        
+        //ArrayList<int> numeros;
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        
+        // agregar elemento
+        nombres.add("Luis");
+        
+        // mostrar
+        System.out.println("Nombre: " + nombres.get(0));
+        
+        // agregar más de un nombre
+        nombres.add("Ana");
+        nombres.add("Diego");
+        nombres.add("Pedro");
+        nombres.add("Adam");
+        
+        System.out.println("Nombre: " + nombres.get(1));
+        System.out.println("Nombre: " + nombres.get(2));
+        System.out.println("Nombre: " + nombres.get(3));
+        System.out.println("Nombre: " + nombres.get(4));
+        
+        // recorre la colección
+        for (String nombre : nombres) {
+            System.out.println("Nombre for: " + nombre);
+        }
+    }
+}
