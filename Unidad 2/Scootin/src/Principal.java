5106a6b4efd1180fc23d1b099f2e585ce811dcde
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-10 12:15:56 -0300

    Update Principal.java

--- Unidad 2/Scootin/src/Principal.java
+++ Unidad 2/Scootin/src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
     public static void main(String[] args) {
         /*
         Ejercicio: agregar empleados, 1 es el gerente y otro es vendedor
-        
+        mostrar la lista de empleados actuales
         */
     }
 }
