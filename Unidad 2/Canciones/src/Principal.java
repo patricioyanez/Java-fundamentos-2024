79dde24857e0f2e74752e26ea4efbba654e3a7a2
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-12 12:29:05 -0300

    update

--- Unidad 2/Canciones/src/Principal.java
+++ Unidad 2/Canciones/src/Principal.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
         ArrayList<Cancion> lista1 = new ArrayList<Cancion>();
         Cancion cancion1 = new Cancion("gasolina", "Daddy", 300, true, true);
         Cancion cancion2 = new Cancion("Compa", "Peso Pluma", 200, true, false);
@@ -15,5 +17,17 @@ public class Principal {
         System.out.println("1.-" + cancion1.imprimir());
         System.out.println("2.-" + cancion2.imprimir());
         System.out.println("3.-" + cancion3.imprimir());
+        System.out.print("Seleccione canción a reproducir: ");
+        int opcion = leer.nextInt();
+        
+        System.out.println("Canción en reproducción");
+        if(opcion == 1)
+            System.out.println(cancion1.imprimir());
+        else if(opcion == 2)
+            System.out.println(cancion2.imprimir());
+        else if(opcion == 3)
+            System.out.println(cancion3.imprimir());
+        else
+            System.out.println("No existe esa opción");
     }
 }
