110219046b4b1b858b48cf1618403a199d945969
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-12 12:23:07 -0300

    update

--- Unidad 2/Canciones/src/Principal.java
+++ Unidad 2/Canciones/src/Principal.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
-   
+        ArrayList<Cancion> lista1 = new ArrayList<Cancion>();
+        Cancion cancion1 = new Cancion("gasolina", "Daddy", 300, true, true);
+        Cancion cancion2 = new Cancion("Compa", "Peso Pluma", 200, true, false);
+        Cancion cancion3 = new Cancion("Bailando solo", "Bunkers", 360, false, false);
+        
+        lista1.add(cancion1);
+        lista1.add(cancion2);
+        lista1.add(cancion3);
+        
+        System.out.println("Lista de canciones");
+        System.out.println("1.-" + cancion1.imprimir());
+        System.out.println("2.-" + cancion2.imprimir());
+        System.out.println("3.-" + cancion3.imprimir());
     }
 }
