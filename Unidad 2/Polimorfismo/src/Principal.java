e4377fd1917f926fb365d1370ddbbb5628f2e9d3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-15 12:20:32 -0300

    update

--- Unidad 2/Polimorfismo/src/Principal.java
+++ Unidad 2/Polimorfismo/src/Principal.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
+        
+        Animal animal = new Animal();
+        Pez atun = new Pez();
+        Ave pollo = new Ave();
+        
+        
+        animal.mover();
+        atun.mover();
+        pollo.mover();
+        
+        Animal animal1 = new Pez();
+        Animal animal2 = new Ave();
+        // crear coleccion
+        ArrayList<Animal> animales;
+        animales = new ArrayList<Animal>();
+        // agregar animal y sus subclases
+        animales.add(animal);
+        animales.add(atun);
+        animales.add(pollo);
+        
+        System.out.println("\n\n*** animales en colección ***");
+        for (Animal a : animales) {
+            a.mover();
+        }
         
     }
 }
