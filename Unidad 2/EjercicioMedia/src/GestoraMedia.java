90624037eef169f9662ae4908b7ece2f31d70adb
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-17 18:30:16 -0300

    Create GestoraMedia.java

---
+++ Unidad 2/EjercicioMedia/src/GestoraMedia.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+
+public class GestoraMedia {
+    ArrayList<Usuario> lista = new ArrayList<Usuario>();
+    
+    public boolean agregar()
+    {
+        return true;
+    }
+    
+}
