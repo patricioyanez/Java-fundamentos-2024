203e6a86e1222da15e0765ead97c169db01fa042
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-17 12:30:30 -0300

    update

--- Unidad 2/EjercicioMedia/src/Principal.java
+++ Unidad 2/EjercicioMedia/src/Principal.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author patri
- */
 public class Principal {
+    
+    // crear un par de objetos (8), crear clase gestora 
+    // y agregar los objetos creados
     
 }
