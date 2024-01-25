9f530f45355cf4238dddc943e491603b23a2e504
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-01-25 11:22:08 -0300

    update

--- Unidad 3/TallerMecanico/src/controlador/ControladorMarca.java
+++ Unidad 3/TallerMecanico/src/controlador/ControladorMarca.java
@@ -1,6 +1,7 @@
 package controlador;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import modelo.Marca;
 
@@ -55,11 +56,58 @@ public class ControladorMarca {
     }
     public Marca buscarPorId(int id)
     {
-        
+       try
+        {
+            Conexion conexion = new Conexion();
+            Connection cx = conexion.obtenerConexion();
+            String sql = "SELECT id, nombre, habilitado FROM marca WHERE ID = ?";
+
+            PreparedStatement st;
+            st = cx.prepareStatement(sql);
+            st.setInt(0, id);
+            
+            ResultSet rs = st.executeQuery();
+            
+            if(rs.next())
+            {
+                Marca marca = new Marca();
+                marca.setId(rs.getInt("id"));
+                marca.setNombre(rs.getString("nombre"));
+                marca.setHabilitado(rs.getInt("habilitado")==1 ? true: false);
+            }
+            
+            st.close();
+            cx.close();
+            return null;
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
+        return null; 
     }
     
     public boolean eliminar(int id)
     {
-        
+       try
+        {
+            Conexion conexion = new Conexion();
+            Connection cx = conexion.obtenerConexion();
+            String sql = "DELETE FROM MARCA WHERE ID = ?";
+
+            PreparedStatement st;
+            st = cx.prepareStatement(sql);
+            st.setInt(0, id);            
+
+            st.executeUpdate();
+            st.close();
+            cx.close();
+            return true;
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
+        return false;     
     }
 }
